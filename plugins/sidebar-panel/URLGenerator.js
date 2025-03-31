137574a0a47a19d373dc7f2f2b48c5f1c4b92cfd
Dalia <dalia.bachmatiuk@codewave.pl>
2025-03-31 14:43:57 +0200

    update link to live preview

--- plugins/sidebar-panel/URLGenerator.js
+++ plugins/sidebar-panel/URLGenerator.js
@@ -21,7 +21,7 @@ export class URLGenerator {
 
   getURL(object) {
     const baseURLInstance = new URL(
-      `${this.config.base_url.replace(/\/+$/, "")}/api/flotiq/draft`,
+      `${this.config.base_url.replace(/\/+$/, "")}/api/flotiq/live-preview`,
     );
     baseURLInstance.searchParams.set("key", this.config.editor_key);
     baseURLInstance.searchParams.set("apiKey", this.config.api_key);
@@ -35,7 +35,7 @@ export class URLGenerator {
       },
     );
 
-    baseURLInstance.searchParams.set("draft", "true");
+    baseURLInstance.searchParams.set("live-preview", "true");
     baseURLInstance.searchParams.set("redirect", `/${path.replace(/^\//, "")}`);
 
     return baseURLInstance;
